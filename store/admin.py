466c56266fa0e2d24f47c72be3df825c8df133fa
SarwarTaki <75441816+SarwarTaki@users.noreply.github.com>
2021-01-02 23:11:10 +0600

    Add files via upload

---
+++ store/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Registering models here.
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
